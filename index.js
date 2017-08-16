d49449c52b5e90153307a95c553309290f939283
eedamame <eedamame@gmail.com>
2017-08-16 15:48:27 +0900

    make project name dir

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const childProcess = require('child_process');
 const fs = require('fs');
+const path = require('path');
 
 const chrome = '"/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"';
-const pagelist = JSON.parse(fs.readFileSync('./siteconfig.json', 'utf8'));
-const basePath = pagelist.basePath;
-const paths = pagelist.pagelist;
+const siteconfig = JSON.parse(fs.readFileSync('./siteconfig.json', 'utf8'));
+const projectName = siteconfig.projectName;
+const basePath = siteconfig.basePath;
+const paths = siteconfig.pagelist;
 
-//const paths = require('./pagelist.json');
-//const paths = [
-//  { 'name': '01_toppage', path: '' },
-//  { 'name': '02_admissions', path: 'admissions/index.php' }
-//];
+
+/* =============================================================================
+   make projectName dir
+============================================================================= */
+if (!fs.existsSync('capture/' + projectName)) {
+  fs.mkdir('capture/' + projectName);
+}
+
+/* =============================================================================
+   run capture
+============================================================================= */
 for (var i = 0; i < paths.length; i++) {
-    childProcess.exec(chrome + ' --headless --disable-gpu --screenshot=.\/capture\/' + paths[i]['name'] + '.png --window-size=1280,4000 ' + basePath + paths[i]['path']);
+    childProcess.exec(chrome + ' --headless --disable-gpu --screenshot=.\/capture\/' + projectName + '/' + paths[i]['name'] + '.png --window-size=1280,4000 ' + basePath + paths[i]['path']);
 }
