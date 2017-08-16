388cec60f4ff599bab11fa78dcd61d64d88512e7
eedamame <eedamame@gmail.com>
2017-08-16 16:24:15 +0900

    make date dir

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const childProcess = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const moment = require('moment');
 
 const chrome = '"/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"';
 const siteconfig = JSON.parse(fs.readFileSync('./siteconfig.json', 'utf8'));
@@ -8,17 +9,48 @@ const projectName = siteconfig.projectName;
 const basePath = siteconfig.basePath;
 const paths = siteconfig.pagelist;
 
+const today = moment(Date.now()).format('YYMMDD');
+
+const projectDir = 'capture/' + projectName;
+const todaysDir = projectDir + '/' + today;
 
 /* =============================================================================
    make projectName dir
 ============================================================================= */
-if (!fs.existsSync('capture/' + projectName)) {
-  fs.mkdir('capture/' + projectName);
+/* ----------------------------------------------
+   make project dir
+---------------------------------------------- */
+const makeProjectDir = () => fs.mkdir(projectDir, function() {
+  console.log('made project dir: `' + projectDir + '`');
+});
+
+/* ----------------------------------------------
+   make todays dir
+---------------------------------------------- */
+const makeTodaysDir = () => fs.mkdir(todaysDir, function() {
+  console.log('made todays dir: `' + todaysDir + '`');
+});
+
+/* ----------------------------------------------
+   check dir
+---------------------------------------------- */
+if (!fs.existsSync(projectDir)) {
+  makeProjectDir();
+  if (!fs.existsSync(todaysDir)) {
+    makeTodaysDir();
+  }
+} else {
+  if (!fs.existsSync(todaysDir)) {
+    makeTodaysDir();
+  }
 }
 
 /* =============================================================================
    run capture
 ============================================================================= */
 for (var i = 0; i < paths.length; i++) {
-    childProcess.exec(chrome + ' --headless --disable-gpu --screenshot=.\/capture\/' + projectName + '/' + paths[i]['name'] + '.png --window-size=1280,4000 ' + basePath + paths[i]['path']);
+  childProcess.exec(
+    chrome + ' --headless --disable-gpu --window-size=1280,4000 --screenshot=.\/' + todaysDir + '/'
+      + paths[i]['name'] + '.png ' + basePath + paths[i]['path']
+  );
 }
