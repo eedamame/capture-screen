cc80ffbd524ab19b55d1eb4ef63f8bddcbac96e1
eedamame <eedamame@gmail.com>
2017-08-22 14:04:42 +0900

    create diff image, and check diff (required imageMagick)

--- index.js
+++ index.js
@@ -8,9 +8,29 @@ const moment = require('moment');
 const chrome = '"/Applications/Chromium.app/Contents/MacOS/Chromium"';
 //const chrome = '"/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"';
 
+
+/* =============================================================================
+ * options
+ * -------
+ * project: (alias: p) project name that uses config file name (siteconfig_PROJECTNAME.json)
+ * compare: (alias: c) target directory compare screenshots 
+ *
+============================================================================= */
+
+const minimist = require('minimist');
+let argv = minimist(process.argv.slice(2), {
+  string: ['project', 'diff'],
+  boolean: [],
+  alias: {
+    p: 'project',
+    c: 'compare'
+  }
+})
+
+
 let configFile = './siteconfig.json';
-if(process.argv[2]) {
-  configFile = './siteconfig_' + process.argv[2] + '.json';
+if(argv.project) {
+  configFile = './siteconfig_' + argv.project + '.json';
 }
 if(!fs.existsSync(configFile)) {
   console.log(configFile + ' does not exist.');
@@ -25,6 +45,15 @@ const paths = siteconfig.pagelist;
 const today = moment(Date.now()).format('YYMMDD');
 const projectDir = 'capture/' + projectName;
 const todaysDir = projectDir + '/' + today;
+const hcOptions = [
+  '--headless',
+  '--disable-gpu',
+  '--allow-insecure-localhost',
+  '--allow-running-insecure-content',
+  '--disable-web-security',
+  '--ignore-certificate-errors',
+  '--window-size=1280,4000'
+];// ooptions for headless chrome
 
 /* =============================================================================
    make projectName dir
@@ -61,8 +90,39 @@ if (!fs.existsSync(projectDir)) {
    run capture
 ============================================================================= */
 for (var i = 0; i < paths.length; i++) {
+  const targetName = paths[i]['name'];
+  const targetURL = basePath + paths[i]['path'];
+  const todaysCapture = todaysDir + '/' + targetName + '.png';
+  const diffImg = todaysDir + '/' + targetName + '_diff.png';
+  let beforeImg;
+  if(argv.compare) {
+    beforeImg = projectDir + '/' + argv.compare + '/' + targetName + '.png';
+  }
+
   childProcess.exec(
-    chrome + ' --headless --disable-gpu --window-size=1280,4000 --allow-insecure-localhost --allow-running-insecure-content --disable-web-security --ignore-certificate-errors --screenshot=.\/' + todaysDir + '/'
-      + paths[i]['name'] + '.png ' + basePath + paths[i]['path']
-  );
+    chrome + ' ' + hcOptions.join(' ') + ' --screenshot=.\/' + todaysCapture + ' ' + targetURL
+  , function(error, stdout, stderr) {
+
+    if(argv.compare) {
+      /* ----------------------------------------------
+       * create diff image
+       * execSyncで書くと、作成したtodaysCaptureを参照できなかったので、callbackで書いてる
+      ---------------------------------------------- */
+      childProcess.exec(
+        'composite -compose difference ' + beforeImg + ' ' + todaysCapture + ' ' + diffImg
+      , function(error, stdout, stderr) {
+        childProcess.exec(
+          'identify -format "%[mean]" ' + diffImg
+        , function (error, stdout, stderr) {
+          if(stdout > 0) {
+            console.log('変更があります: ' + targetName);
+          }
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+        });
+      });
+    }
+
+  });
 }
